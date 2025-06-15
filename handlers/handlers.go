48699aabba6f4518e708d3d9059af2749de2c03e
kodaiozekijp <kodai.ozeki@gmail.com>
2025-06-15 22:42:38 +0900

    added article's query and path parameter

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 // ハンドラ定義
@@ -20,12 +23,34 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 
 // ArticleListHandler
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	// URLからクエリパラメータを取得
+	queryMap := req.URL.Query()
+	// ページ番号に該当するデータを返す
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+		// クエリパラメータがURLに含まれていない場合は、1ページ目のデータを返す
+	} else {
+		page = 1
+	}
+
+	resString := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(w, resString)
 }
 
 // ArticleDetailHandler
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	articleID := 1
+	// URLからパスパラメータである記事IDを取得し、該当する記事を返す
+	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
+	if err != nil {
+		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
+		return
+	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
 }
