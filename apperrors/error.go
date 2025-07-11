aaf034536c64d4ba5e22ddc68d0d95fc41490381
kodaiozekijp <kodai.ozeki@gmail.com>
2025-07-12 00:01:16 +0900

    UPDATE:「error.go」のMyAppError構造体のフィールド及びメソッドを追加。

--- apperrors/error.go
+++ apperrors/error.go
@@ -1,6 +1,15 @@
 package apperrors
 
 type MyAppError struct {
-	ErrCode
-	Message string
+	ErrCode        // レスポンスとログに表示するエラーコード
+	Message string // レスポンスに表示するエラーメッセージ
+	Err     error  // エラーチェーンの為の内部エラー
+}
+
+func (myErr *MyAppError) Error() string {
+	return myErr.Err.Error()
+}
+
+func (myErr *MyAppError) UnWrap() error {
+	return myErr.Err
 }
