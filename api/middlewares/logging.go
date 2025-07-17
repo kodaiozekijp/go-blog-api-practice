9af7bd3f6a08f33ae9cbeab8ed5e2477772d36af
kodaiozekijp <kodai.ozeki@gmail.com>
2025-07-17 23:29:01 +0900

    UPDATE:「logging.go」に自作のResponseWriterであるresLoggingWriter構造体を定義。当該構造体を利用して、レスポンス時のcodeをロギング。

--- api/middlewares/logging.go
+++ api/middlewares/logging.go
@@ -5,12 +5,37 @@ import (
 	"net/http"
 )
 
+// 自作のResponseWriter
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+// コンストラクタを作る
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+// WriteHeaderメソッドのオーバーライド
+func (rlw *resLoggingWriter) WriteHeader(code int) {
+	rlw.code = code
+	rlw.ResponseWriter.WriteHeader(code)
+}
+
 func LoggingMiddleWare(next http.Handler) http.Handler {
 	// http.Handlerを返却する
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		// リクエスト情報をロギング
 		log.Println(req.RequestURI, req.Method)
+
+		// 自作のResponseWriterを生成
+		rlw := NewResLoggingWriter(w)
+
 		// リクエストを処理
-		next.ServeHTTP(w, req)
+		next.ServeHTTP(rlw, req)
+
+		// 自作ResponseWriterからロギングしたいデータを取得する
+		log.Println("response-code: ", rlw.code)
+
 	})
 }
